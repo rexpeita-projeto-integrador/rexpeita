a48f9d52801442538d219dac547c81d5c80685ab
Yuri Portela Teixeira <53837552+YuriPortela@users.noreply.github.com>
2019-10-19 20:22:31 -0300

    Rexpeita V2.0 - MVC
    Adapted into MVC:
    - Index page
    - Login page
    - Account Creation page
    
    Co-Authored-By: dreeeik <dreeeik@users.noreply.github.com>

---
+++ models/products.php
@@ -0,0 +1,75 @@
+<?php
+class Products extends model {
+
+	public function getList($offset = 0, $limit = 3) {
+		$array = array();
+
+		$sql = "SELECT *,
+		( select brands.name from brands where brands.id = products.id_brand ) as brand_name,
+		( select categories.name from categories where categories.id = products.id_category ) as category_name
+		FROM products LIMIT $offset, $limit";
+		$sql = $this->db->query($sql);
+
+		if($sql->rowCount() > 0) {
+
+			$array = $sql->fetchAll();
+
+			foreach($array as $key => $item) {
+
+				$array[$key]['images'] = $this->getImagesByProductId($item['id']);
+
+			}
+
+
+		}
+
+		return $array;
+	}
+
+	public function getTotal() {
+		$sql = "SELECT COUNT(*) as c FROM products";
+		$sql = $this->db->query($sql);
+		$sql = $sql->fetch();
+
+		return $sql['c'];
+	}
+
+	public function getImagesByProductId($id) {
+		$array = array();
+
+		$sql = "SELECT url FROM products_images WHERE id_product = :id";
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(":id", $id);
+		$sql->execute();
+
+		if($sql->rowCount() > 0) {
+			$array = $sql->fetchAll();
+		}
+
+		return $array;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
