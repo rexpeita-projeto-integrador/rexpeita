83064a9c8049663c47a518f804bb423956ec49f1
yuricosta-wq <51718018+yuricosta-wq@users.noreply.github.com>
2019-10-29 17:56:08 -0300

    tentativa fracssada

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,6 +2,6 @@
 class loginController extends controller {
     
     public function index() {
-        $this->loadTemplate('login');
+        $this->loadView('login');
     }
 }
--- views/login_proc.php
+++ views/login_proc.php
@@ -1,28 +1,20 @@
 <?php
 
-session_start();
+$mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
 
-$con = mysqli_connect('localhost','root','');
-
-mysqli_select_db($con, 'rexpeita');
-
-$id = $_POST['id'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$s = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
+$mysqli->query("SELECT email, password  FROM users where '$email' = email and '$password' = password  ") or die($mysqli->error);
 
-$result = mysqli_query($con, $s);
-
-$num = mysqli_num_rows($result);
-
-if($num == 1) {
-    $_SESSION['email'] = $email;
-    $_SESSION['password'] = $password;
-    $_SESSION['id'] = $id;
-    header('location:home.php');
-} else {
-    header('location:login.php');
+if($email == $mysqli || $password == $mysqli )
+{
+    header('Location:home.php');
 }
+else
+{
+    header('Location:login.php');
+}
+
     
 ?>
