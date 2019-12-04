90c764aedebceee9920cbc57fc8c0d88682e2c55
yuricosta-wq <51718018+yuricosta-wq@users.noreply.github.com>
2019-12-04 15:02:27 -0300

    tentativa de hash1(sha1)

--- views/login_proc.php
+++ views/login_proc.php
@@ -3,10 +3,21 @@
 $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
 
 $email = $_POST['email'];
-$password = $_POST['password'];
-$password = hash("sha256", $password);
+$senha = $_POST['pass'];
+$pass = "";
 
-$login = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
+
+try
+{
+    $pass = sha1($senha);
+    $sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$pass'";
+    $sql = $pdo->query($sql);
+
+}
+
+
+
+$login = "SELECT * FROM users WHERE email = '$email' && password = '$senha' ";
 $verifica = "SELECT * FROM users WHERE administra = '$verifica' ";
 $resultverifica = mysqli_query($mysqli, $verifica);
 
