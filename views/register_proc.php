7b53735c766080ec3c85f9ab141af57cdc9b62a0
yuricosta-wq <51718018+yuricosta-wq@users.noreply.github.com>
2019-12-05 15:12:44 -0300

    Update register_proc.php

--- views/register_proc.php
+++ views/register_proc.php
@@ -4,8 +4,44 @@ $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mys
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-$password = hash("sha256", $password);
+$pass = "";
 
-$mysqli->query("INSERT INTO users (email, password) VALUES('$email', '$password')") or die($mysqli->error);
+
+
+
+try {
+    $pass = sha1($senha);
+    $sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$pass'";
+    $sql = $pdo->query($sql);
+    
+    echo "Dados inseridos com sucesso: ".$pdo->lastInsertId();
+    } catch(PDOException $e) {
+        
+        echo "Error ". $e->getMessage();
+    }
+
+    $login = "SELECT * FROM users WHERE email = '$email' && password = '$senha' ";
+    $verifica = "SELECT * FROM users WHERE administra = '$verifica' ";
+    $resultverifica = mysqli_query($mysqli, $verifica);
+    
+    $result = mysqli_query($mysqli, $login);
+    
+    $num = mysqli_num_rows($result);
+    
+    if($num == 1) {
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+            if($resultverifica == 1)
+            {
+                header("Location:myaccountadm");
+            } else {
+                header("Location:home");
+            }
+    } else {
+        unset($_SESSION['email']);
+        unset($_SESSION['password']);
+        header('location:login');
+    }
+    
     
 ?>
