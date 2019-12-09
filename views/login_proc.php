66d8e9329e55ce07fc4f65c57581dea9f186e22e
yuricosta-wq <51718018+yuricosta-wq@users.noreply.github.com>
2019-12-09 16:15:26 -0300

    vou apagar o outro branch

--- views/login_proc.php
+++ views/login_proc.php
@@ -1,6 +1,8 @@
 <?php
 
 $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
+  
+
 
 $email = $_POST['email'];
 $password = md5($_POST['password']);
--- views/register_proc.php
+++ views/register_proc.php
@@ -2,6 +2,8 @@
 
 $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
 
+
+
 $email = $_POST['email'];
 $password = md5($_POST['password']);
 
