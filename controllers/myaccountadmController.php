e1c6207f3e875a503c78f324901bc2ea3452f7e7
Yuri Portela Teixeira <53837552+YuriPortela@users.noreply.github.com>
2019-12-10 16:07:12 -0300

    Senha MD5 funcionando
    * Login
    * Cadastro

--- controllers/myaccountadmController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class myaccountadmController extends controller {
-    
-    public function index() {
-        $this->loadTemplate('myaccountadm');
-    }
-}
--- views/login_proc.php
+++ views/login_proc.php
@@ -1,15 +1,14 @@
 <?php
 
 $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
-  
 
+$email = mysqli_real_escape_string($mysqli, $_POST["email"]);  
+$password = mysqli_real_escape_string($mysqli, $_POST["password"]);  
+$password = md5($password); 
 
-$email = $_POST['email'];
-$password = md5($_POST['password']);
+$query = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
 
-$login = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
-
-$result = mysqli_query($mysqli, $login);
+$result = mysqli_query($mysqli, $query);
 
 $num = mysqli_num_rows($result);
 
