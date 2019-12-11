c49c0147ef457e22ac06d54b26fb0db7f778a183
Yuri Portela Teixeira <53837552+YuriPortela@users.noreply.github.com>
2019-12-10 23:22:35 -0300

    resolvendo conflitos 2

--- views/login_proc.php
+++ views/login_proc.php
@@ -2,25 +2,31 @@
 
 $mysqli = new mysqli('localhost','root','', 'rexpeita') or die(mysqli_error($mysqli));
 
-$email = mysqli_real_escape_string($mysqli, $_POST["email"]);  
-$password = mysqli_real_escape_string($mysqli, $_POST["password"]);  
-$password = md5($password); 
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
+$login = "SELECT * FROM users WHERE email = '$email' && password = '$password' ";
+$verifica = "SELECT * FROM users WHERE administra = '$verifica' ";
+$resultverifica = mysqli_query($mysqli, $verifica);
 
-$result = mysqli_query($mysqli, $query);
+$result = mysqli_query($mysqli, $login);
 
 $num = mysqli_num_rows($result);
 
 if($num == 1) {
     $_SESSION['email'] = $email;
     $_SESSION['password'] = $password;
-    header('location:myaccount');
-    
+        if($resultverifica == 1)
+        {
+            header("Location:myaccountadm");
+        } else {
+            header("Location:home");
+        }
 } else {
     unset($_SESSION['email']);
     unset($_SESSION['password']);
     header('location:login');
 }
-    
+
+
 ?>
